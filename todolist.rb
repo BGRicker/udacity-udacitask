4573d957e716939f8ab52db69759fded8b8cc495
Ben Ricker <BGRicker@gmail.com>
2016-07-02 23:42:23 -0400

    todo list class and methods

--- todolist.rb
+++ todolist.rb
@@ -1,7 +1,18 @@
 class TodoList
-    # methods and stuff go here
+    attr_accessor :title, :list
+
+    def initialize
+      @list = []
+    end
+
+    def add_item(item)
+      @list << item
+    end
+
+    def remove_item(item)
+      @list.delete(item)
+    end
 end
 
 class Item
-    # methods and stuff go here
 end
--- udacitask.rb
+++ udacitask.rb
@@ -1,18 +1,28 @@
 require_relative 'todolist.rb'
 
 # Creates a new todo list
+todo = TodoList.new
 
 # Add four new items
+todo.add_item "eat pizza"
+todo.add_item "drink coffee"
+todo.add_item "watch movie"
+todo.add_item "go to sleep"
 
 # Print the list
+print todo.list
 
 # Delete the first item
+todo.remove_item("eat pizza")
 
 # Print the list
+print todo.list
 
 # Delete the second item
+todo.remove_item("watch movie")
 
 # Print the list
+print todo.list
 
 # Update the completion status of the first item to complete
 
