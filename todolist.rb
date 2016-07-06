5a0aff58c97a0b38c5f6421f8e08a9d76687b91b
Ben Ricker <BGRicker@gmail.com>
2016-07-06 19:26:43 -0400

    refactor, two features

--- todolist.rb
+++ todolist.rb
@@ -1,31 +1,69 @@
 class TodoList
-    attr_accessor :title, :list
+  attr_accessor :title
+  attr_reader :list
 
-    def initialize(title)
-      @list = []
-      @title = title
-    end
+  def initialize(title)
+    @list = []
+    @title = title
+  end
 
-    def add_item(item)
-      @list << item
-    end
+  def add_item(item)
+    @list << item
+  end
 
-    def remove_item(item)
-      @list.delete(item)
-    end
+  def rename_list(title)
+    @title = title
+  end
 
-    def print_list
-      @list.map(&:task)
+  # def add_item(new_item)
+  #   item = Item.new(new_item)
+  #   @items.push(item)
+  # end
+
+  def remove_item(item)
+    @list.delete(item)
+  end
+
+  def list
+    @list.map(&:task)
+  end
+
+  def print_list
+    puts title
+    puts "-"*10
+    puts list
+    puts
+  end
+
+  def magic_list(string)
+    a = string.split(", ")
+    a.each do |i|
+      @list << Item.new("#{i}")
     end
-    
+  end
+
+  def numbered_print
+    list.each_with_index do |i, n|
+      n = n + 1
+      puts "#{n} - #{i}"
+    end
+  end
+
+  def fancy_print
+    puts title
+    puts "-"*10
+    numbered_print
+    puts
+  end
+
 end
 
 class Item
-  attr_accessor :task, :complete
-  
-  def initialize(task, complete = false)
+  attr_reader :task, :complete
+
+  def initialize(task)
     @task = task
-    @complete = complete
+    @complete = false
   end
 
   def complete
--- udacitask.rb
+++ udacitask.rb
@@ -39,7 +39,33 @@ puts todo.print_list
 puts
 
 # Update the title of the list
-todo.title = "New To-Do"
+todo.rename_list "New To-Do"
+
+print todo.numbered_print
 
 # Print the list
 print todo.print_list
+
+
+
+# Fancy features
+
+
+# numbered print
+# prints your list with numbered prioritization
+pizza = TodoList.new("Pizza List")
+eat = Item.new("eat pizza")
+drink = Item.new("drink coffee")
+watch = Item.new("watch movie")
+nap = Item.new("go to sleep")
+pizza.add_item eat
+pizza.add_item drink
+pizza.add_item watch
+pizza.add_item nap
+pizza.fancy_print
+
+# quick list
+# create a quick list, items split up by commas
+cool_list = TodoList.new("A cool list")
+cool_list.magic_list("go to the store, buy a pizza, cook the pizza, eat the pizza")
+cool_list.fancy_print
